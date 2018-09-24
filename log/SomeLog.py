e83785d36c0f8233b7a67b81024f15abe45b5364
xuetao <xuetaopwd>
2018-09-24 23:15:44 +0800

    log use level

---
+++ log/SomeLog.py
@@ -0,0 +1,10 @@
+import logging
+
+# logging.basicConfig(level=logging.ERROR)
+# 日志默认级别为warning
+logging.debug('debug message')
+logging.info('info message')
+logging.warning('warning message')
+logging.error('error message')
+logging.critical('critical message')
+
